9e4f16ffd0c336c29b113ace4080438c30a29867
laguilar17 <larry_aguilar@rocketmail.com>
2022-07-25 14:34:00 -0500

    rspec pending change

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "Homes", type: :request do
-  describe "GET /index" do
+  pending describe "GET /index" do
     it "returns http success" do
       get "/home/index"
       expect(response).to have_http_status(:success)
